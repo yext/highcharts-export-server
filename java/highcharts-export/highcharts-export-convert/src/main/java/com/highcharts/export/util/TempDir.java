240901beb09e3cb3139782b546dcbdc1c6d6b9e9
Avneesh Sarwate <asarwate@yext.com>
2016-05-04 16:30:26 -0400

    adding yext specific changes from old highcharts repo as of april 21, 2016

--- java/highcharts-export/highcharts-export-convert/src/main/java/com/highcharts/export/util/TempDir.java
+++ java/highcharts-export/highcharts-export-convert/src/main/java/com/highcharts/export/util/TempDir.java
@@ -9,6 +9,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
@@ -28,12 +31,14 @@ public class TempDir {
 	
 	
 	public TempDir() throws IOException {
-		tmpDir = Files.createTempDirectory("export");
+		Set<PosixFilePermission> perms =
+ 		    PosixFilePermissions.fromString("rwxr-xr--");
+ 		tmpDir = Files.createTempDirectory("export", PosixFilePermissions.asFileAttribute(perms));
 
 		// Delete this directory on deletion of the JVM
 		tmpDir.toFile().deleteOnExit();
 
-		outputDir = Files.createDirectory(Paths.get(tmpDir.toString(), "output"));
+		outputDir = Files.createDirectory(Paths.get(tmpDir.toString(), "output"), PosixFilePermissions.asFileAttribute(perms));
 		outputDir.toFile().deleteOnExit();
 		
 		phantomJsDir = Files.createDirectory(Paths.get(tmpDir.toString(), "phantomjs"));
